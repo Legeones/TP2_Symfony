acb34b8477b2273bf7747fe1bad7243d80412ce8
Théo Bernaville <92569493+Legeones@users.noreply.github.com>
2024-10-24 15:59:29 +0200

    Dashboard with bootstrap

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,14 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\TicketRepository;
 
 class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(): Response
+    public function index(TicketRepository $ticketRepository): Response
     {
         return $this->render('dashboard/index.html.twig', [
+            'tickets' => $ticketRepository->findAll(),
             'controller_name' => 'DashboardController',
         ]);
     }
+
+
 }
